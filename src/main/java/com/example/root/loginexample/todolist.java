8a62ae258a6b3f00659d615dba6169881b5a45ab
Abhishek Singhal <Abhisheksinghalx11@gmail.com>
2017-09-29 14:26:02 +0530

    Update todolist.java

--- src/main/java/com/example/root/loginexample/todolist.java
+++ src/main/java/com/example/root/loginexample/todolist.java
@@ -17,9 +17,8 @@ public class todolist extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todolist);
         ListView lstTask =  (ListView) findViewById(R.id.tlist);
-        //TextView tText = (TextView)findViewById(R.id.ttext);
         List<String> taskList =  helper.getTaskList();
-        ArrayAdapter<String>  mAdapter = new ArrayAdapter<String>(this,R.layout.activity_todolist,R.id.ttext,taskList);
+        ArrayAdapter<String>  mAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1 ,taskList);
         lstTask.setAdapter(mAdapter);
 
     }
